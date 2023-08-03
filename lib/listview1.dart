97446529124f8e4f6c262f75cf754a97271935da
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-08-03 14:10:20 +0530

    commit

---
+++ lib/listview1.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ListView1(),
+  ));
+}
+
+class ListView1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ListView"),
+      ),
+      body: ListView(
+        children:  [
+          Card(
+            child: ListTile(
+              title: Text("HamBurger"),
+              subtitle: Text("\$190"),
+              trailing: Icon(Icons.shopping_cart),
+              leading: Image.asset("assets/images/hamburger.png") ,
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text("Pizza"),
+              subtitle: Text("\$290"),
+              trailing: Icon(Icons.shopping_cart),
+              leading: Image.asset("assets/images/pizza.png") ,
+            ),
+          ),
+          Text("Item 3"),
+          Text("Item 4"),
+          Text("Item 5"),
+        ],
+      ),
+    );
+  }
+}
