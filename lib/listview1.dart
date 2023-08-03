c326c7e49a6a817a82c823620db8c7d96daea084
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-08-03 14:48:00 +0530

    commit

--- lib/listview1.dart
+++ lib/listview1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -11,32 +12,53 @@ class ListView1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("ListView"),
+        title: const Text("ListView"),
       ),
       body: ListView(
-        children:  [
+        children: [
+          Center(
+              child: Text(
+            "Select Your Food From the menu!!",
+            style: GoogleFonts.pacifico(
+                fontSize: 30, color: Colors.green, fontWeight: FontWeight.bold),
+          )),
           Card(
             child: ListTile(
-              title: Text("HamBurger"),
-              subtitle: Text("\$190"),
-              trailing: Icon(Icons.shopping_cart),
-              leading: Image.asset("assets/images/hamburger.png") ,
+              title: const Text("HamBurger"),
+              subtitle: const Text("\$190"),
+              trailing: const Icon(Icons.shopping_cart),
+              leading: Image.asset("assets/images/hamburger.png"),
             ),
           ),
           Card(
             child: ListTile(
-              title: Text("Pizza"),
-              subtitle: Text("\$290"),
-              trailing: Icon(Icons.shopping_cart),
-              leading: Image.asset("assets/images/pizza.png") ,
+              title: const Text("Pizza"),
+              subtitle: const Text("\$290"),
+              trailing: const Icon(Icons.shopping_cart),
+              leading: Image.asset("assets/images/pizza.png"),
             ),
           ),
           Card(
             child: ListTile(
-              title: Text("Pizza"),
-              subtitle: Text("\$290"),
-              trailing: Icon(Icons.shopping_cart),
-              leading: Image.asset("assets/images/pizza.png") ,
+              title: const Text("Salad"),
+              subtitle: const Text("\$90"),
+              trailing: const Icon(
+                Icons.shopping_cart,
+                color: Colors.teal,
+              ),
+              leading: Image.asset("assets/images/salad.png"),
+            ),
+          ),
+          const Card(
+            child: ListTile(
+              title: Text("Biriyani"),
+              subtitle: Text("\$120"),
+              trailing: Icon(
+                Icons.shopping_cart,
+                color: Colors.teal,),
+              leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://t3.ftcdn.net/jpg/04/59/27/88/360_F_459278894_92eSlejnR7NSwJRCbVyy9ZZibSmjbF8q.jpg")),
             ),
           ),
         ],
