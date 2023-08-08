a329669dd9583185c448babbcc3077c79750cd57
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-08-08 14:58:09 +0530

    commit

--- lib/Gridex/sample ui using gridview.dart
+++ lib/Gridex/sample ui using gridview.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home: SampleGridUi(),));
+void main() {
+  runApp(MaterialApp(
+    home: SampleGridUi(),
+  ));
 }
-class SampleGridUi extends StatelessWidget {
 
-  var images= [
+class SampleGridUi extends StatelessWidget {
+  var images = [
     "https://plus.unsplash.com/premium_photo-1664356382524-80683e066a3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=386&q=80",
     "https://images.unsplash.com/photo-1656133440919-0775929dc15a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
     "https://plus.unsplash.com/premium_photo-1664356382524-80683e066a3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=386&q=80",
@@ -17,31 +19,41 @@ class SampleGridUi extends StatelessWidget {
     "https://plus.unsplash.com/premium_photo-1664356382524-80683e066a3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=386&q=80",
     "https://images.unsplash.com/photo-1656133440919-0775929dc15a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
   ];
-  var color=[Colors.purple,Colors.indigo,Colors.blue,Colors.green,Colors.yellow,Colors.orange,Colors.red,];
+  var color = [
+    Colors.purple,
+    Colors.indigo,
+    Colors.blue,
+    Colors.green,
+    Colors.yellow,
+    Colors.orange,
+    Colors.red,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(18.0),
         child: GridView.builder(
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
               mainAxisSpacing: 10,
               crossAxisSpacing: 5),
           itemBuilder: (context, index) {
             return Container(
               height: 20,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(60),
-                color: color[index]
+                  borderRadius: BorderRadius.circular(60), color: color[index]
                   // image: DecorationImage(
                   //     image: NetworkImage(images[index]))
-              ),
+                  ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Icon(Icons.ac_unit),
-                  SizedBox(width: 15,),
+                  SizedBox(
+                    width: 15,
+                  ),
                   Text("Hello"),
                 ],
               ),
@@ -53,3 +65,5 @@ class SampleGridUi extends StatelessWidget {
     );
   }
 }
+
+
