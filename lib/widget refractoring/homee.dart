7a2ba73db1ea808b410c5f8c482ce7c912f48f6f
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-08-22 15:01:39 +0530

    commit

--- lib/widget refractoring/homee.dart
+++ lib/widget refractoring/homee.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_june1/widget%20refractoring/NewWidget.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -7,6 +8,32 @@ void main() {
 }
 
 class RefraHome extends StatelessWidget {
+  var name = [
+    "food1",
+    "food2",
+    "food3",
+    "food4",
+    "food5",
+    "food6",
+    "food7",
+    "food8",
+    "food9",
+    "food10"
+  ];
+  var image = [
+    "assets/images/donut.png",
+    "assets/images/hamburger.png",
+    "assets/images/pizza.png",
+    "assets/images/ramen.png",
+    "assets/images/salad.png",
+    "assets/images/grapes.png",
+    "assets/images/mango.png",
+    "assets/images/donut.png",
+    "assets/images/hamburger.png",
+    "assets/images/pizza.png"
+  ];
+  var price = [250, 300, 400, 590, 120, 58, 340, 230, 100, 160];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,50 +41,17 @@ class RefraHome extends StatelessWidget {
         title: const Text("GridView Using Refactoring"),
       ),
       body: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2),
+          itemCount: 10,
           itemBuilder: (context, index) {
-            return NewWidget();
+            return NewWidget(
+              myimage: AssetImage(image[index]),
+              name: name[index],
+              price:'${price[index]}',
+            );
           }),
-    );
-  }
-}
-
-class NewWidget extends StatelessWidget {
-  const NewWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset(
-            "assets/images/donut.png",
-            height: 100,
-          ),
-          const Text("Donut"),
-          const Text("\$ 100"),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.favorite),
-                  label: Text('WishList')),
-              const SizedBox(
-                width: 10,
-              ),
-              ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.shopping_cart),
-                  label: const Text('Buy Now')),
-            ],
-          )
-        ],
-      ),
     );
   }
 }
