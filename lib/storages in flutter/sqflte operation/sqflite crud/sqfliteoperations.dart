b643578442685968b1e574306d5915f68abd29a6
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-09-11 15:00:25 +0530

    commit

---
+++ lib/storages in flutter/sqflte operation/sqflite crud/sqfliteoperations.dart
@@ -0,0 +1,30 @@
+import 'package:sqflite/sqflite.dart' as sql;
+
+class SQLHelper {
+  //database creation
+  static Future<sql.Database> createDB() async {
+    return sql.openDatabase('mycontacts.db',
+        version: 1,
+        onCreate: (sql.Database database, int version) async {
+      await createTable(database);
+    });
+  }
+
+  //table for storing values in this db
+  static Future<void> createTable(sql.Database database) async {
+    await database.execute("""CREATE TABLE contacts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    cname TEXT,
+    cnumber TEXT,
+    )""");
+  }
+
+  // save name and phone number to contacts table
+  static Future<int> create_contact(String name, String phonenum) async{
+    final db = await SQLHelper.createDB(); // open database
+    final data = {'cname':name,'cnumber':phonenum};
+    //         insert to a particular table with values as map
+    final id = await db.insert('contacts', data);
+    return id;
+  }
+}
