f69f99ba637d030dbfaec56bc0d5d897c573f68b
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-09-11 15:21:44 +0530

    commit

--- lib/storages in flutter/sqflte operation/sqflite crud/sqfliteoperations.dart
+++ lib/storages in flutter/sqflte operation/sqflite crud/sqfliteoperations.dart
@@ -3,8 +3,7 @@ import 'package:sqflite/sqflite.dart' as sql;
 class SQLHelper {
   //database creation
   static Future<sql.Database> createDB() async {
-    return sql.openDatabase('mycontacts.db',
-        version: 1,
+    return sql.openDatabase('mycontacts.db', version: 1,
         onCreate: (sql.Database database, int version) async {
       await createTable(database);
     });
@@ -13,16 +12,17 @@ class SQLHelper {
   //table for storing values in this db
   static Future<void> createTable(sql.Database database) async {
     await database.execute("""CREATE TABLE contacts(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    cname TEXT,
-    cnumber TEXT,
-    )""");
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        cname TEXT,
+        cnumber TEXT,
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )""");
   }
 
   // save name and phone number to contacts table
-  static Future<int> create_contact(String name, String phonenum) async{
+  static Future<int> create_contact(String name, String phonenum) async {
     final db = await SQLHelper.createDB(); // open database
-    final data = {'cname':name,'cnumber':phonenum};
+    final data = {'cname': name, 'cnumber': phonenum};
     //         insert to a particular table with values as map
     final id = await db.insert('contacts', data);
     return id;
