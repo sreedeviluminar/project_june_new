ff77f5176e4e4eeade79f63dbe3f664c65dc7172
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-09-12 15:45:45 +0530

    commit

--- lib/storages in flutter/sqflte operation/sqflite crud/mainUi.dart
+++ lib/storages in flutter/sqflte operation/sqflite crud/mainUi.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:project_june1/storages%20in%20flutter/sqflte%20operation/sqflite%20crud/sqfliteoperations.dart';
 
-void main(){
-   runApp(MaterialApp(home: SqfliteHome(),));
+void main() {
+  runApp(MaterialApp(
+    home: SqfliteHome(),
+  ));
 }
 
 class SqfliteHome extends StatefulWidget {
@@ -17,6 +19,12 @@ class _SqfliteHomeState extends State<SqfliteHome> {
   List<Map<String, dynamic>> contacts = [];
 
   @override
+  void initState() {
+    loadUi();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -28,7 +36,25 @@ class _SqfliteHomeState extends State<SqfliteHome> {
               itemCount: contacts.length,
               itemBuilder: (context, index) {
                 return Card(
-                  child: ListTile(),
+                  child: ListTile(
+                    title: Text(contacts[index]['cname']),
+                    // accessing single map from a list
+                    subtitle: Text(contacts[index]['cnumber']),
+                    trailing: Wrap(
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              showSheet(contacts[index]['id']);
+                            },
+                            icon: Icon(Icons.edit)),
+                        IconButton(
+                            onPressed: () {
+                              deleteContact(contacts[index]['id']);
+                            },
+                            icon: Icon(Icons.delete))
+                      ],
+                    ),
+                  ),
                 );
               }),
       floatingActionButton: FloatingActionButton(
@@ -43,6 +69,12 @@ class _SqfliteHomeState extends State<SqfliteHome> {
   final phone_cntrl = TextEditingController();
 
   void showSheet(int? id) async {
+    if (id != null) {
+      final existingcontact =
+          contacts.firstWhere((element) => element['id'] == id);
+      name_cntrl.text = existingcontact['cname'];
+      phone_cntrl.text = existingcontact['cnumber'];
+    }
     showModalBottomSheet(
         context: context,
         isScrollControlled: true,
@@ -52,16 +84,14 @@ class _SqfliteHomeState extends State<SqfliteHome> {
                 left: 15,
                 right: 15,
                 top: 15,
-                bottom: MediaQuery.of(context).
-                viewInsets.bottom + 120),
+                bottom: MediaQuery.of(context).viewInsets.bottom + 120),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 TextField(
                   controller: name_cntrl,
                   decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Name"),
+                      border: OutlineInputBorder(), hintText: "Name"),
                 ),
                 const SizedBox(
                   height: 10,
@@ -69,31 +99,56 @@ class _SqfliteHomeState extends State<SqfliteHome> {
                 TextField(
                   controller: phone_cntrl,
                   decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Phone Number"),
+                      border: OutlineInputBorder(), hintText: "Phone Number"),
                 ),
                 const SizedBox(
                   height: 10,
                 ),
                 ElevatedButton(
-                    onPressed: () async{
-                      if(id == null){
+                    onPressed: () async {
+                      if (id == null) {
                         await createContact();
                       }
-                      if(id != null){
-                       // await updateContact(id);
+                      if (id != null) {
+                        await updateContact(id);
                       }
+                      name_cntrl.text = "";
+                      phone_cntrl.text = "";
+                      Navigator.pop(context);
                     },
-                    child: Text(id == null ?
-                    "Create Contact" : "Update Contact"))
+                    child:
+                        Text(id == null ? "Create Contact" : "Update Contact"))
               ],
             ),
           );
         });
   }
+
 //to add a new data or contact to sqflite db
   Future<void> createContact() async {
-   var id = await SQLHelper.create_contact(name_cntrl.text,phone_cntrl.text);
-   print(id);
+    await SQLHelper.create_contact(name_cntrl.text, phone_cntrl.text);
+    loadUi(); // refresh list each time
+  }
+
+  void loadUi() async {
+    final data = await SQLHelper.readContacts();
+    setState(() {
+      contacts =
+          data; // we are performing crud in this list so it must be inside setstate
+      isLoading = false;
+    });
+  }
+
+  Future<void> updateContact(int id) async {
+    await SQLHelper.updateContact(id, name_cntrl.text, phone_cntrl.text);
+    loadUi();
+  }
+
+  Future<void> deleteContact(int id) async{
+    await SQLHelper.deleteContact(id);
+    loadUi();
+    ScaffoldMessenger.of(context).
+    showSnackBar(SnackBar(
+        content: Text("Succesfully Deleted")));
   }
 }
