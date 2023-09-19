54b9155bee01afe7d0e7699f8bb743346dda2bc3
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-09-19 15:38:54 +0530

    commit

--- lib/storages in flutter/hive crud/hive_crud_operations.dart
+++ lib/storages in flutter/hive crud/hive_crud_operations.dart
@@ -16,7 +16,13 @@ class Hive_crud extends StatefulWidget {
 
 class _Hive_crudState extends State<Hive_crud> {
   List<Map<String, dynamic>> task = [];
+  final mybox =Hive.box('to_do Box');
 
+  @override
+  void initState() {
+    load_or_read_Task();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,10 +36,11 @@ class _Hive_crudState extends State<Hive_crud> {
           : ListView.builder(
               itemCount: task.length,
               itemBuilder: (ctx, index) {
+                final mytask = task[index]; //fetch each single map from list
                 return Card(
                   child: ListTile(
-                    title: const Text(""),
-                    subtitle: const Text(""),
+                    title:  Text(task[index]['taskname']),
+                    subtitle:  Text(mytask['taskcont']),
                     trailing: Wrap(
                       children: [
                         IconButton(
@@ -53,6 +60,9 @@ class _Hive_crudState extends State<Hive_crud> {
     );
   }
 
+  final task_controller = TextEditingController();
+  final content_controller = TextEditingController();
+
   void showTask(BuildContext context, int? itemkey) {
     // itemkey issimilar to id in sqflite
     showModalBottomSheet(
@@ -68,24 +78,63 @@ class _Hive_crudState extends State<Hive_crud> {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                const TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Task Name"),
+                TextField(
+                  controller: task_controller,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: "Task Name"),
                 ),
                 const SizedBox(height: 15),
-                const TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Task Content"),
+                TextField(
+                  controller: content_controller,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: "Task Content"),
                 ),
                 ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      if(task_controller.text!="" && content_controller.text!=""){
+                        if(itemkey == null){
+                          createTask({
+                            'name':task_controller.text.trim(),
+                            'content':content_controller.text.trim()
+                          });
+                        }else{
+                         updateTaks(itemkey,{
+                           'name':task_controller.text.trim(),
+                           'content':content_controller.text.trim()
+                         });
+                        }
+                      }
+                      content_controller.text="";
+                      task_controller.text = "";
+                      Navigator.of(context).pop();
+                    },
                     child:
                         Text(itemkey == null ? 'Create Task' : 'Update Task'))
               ],
             ),
           );
         });
+  }
+  Future<void> createTask(Map<String, dynamic> task) async{
+    await mybox.add(task);
+    load_or_read_Task();
+  }
+
+  void updateTaks(int? itemkey, Map<String, String> map) {}
+
+  void load_or_read_Task() {
+    final task_from_hive = mybox.keys.map((key) {
+      //fetch all the keys from hive in ascending order
+      final value = mybox.get(key); // all the values of each individual key from the box
+      return{
+        'id' :key,
+        'taskname':value['name'],
+        'taskcont':value['content']
+      };
+    }).toList();
+
+    setState(() {
+      task = task_from_hive.reversed.toList();
+    });
   }
 }
